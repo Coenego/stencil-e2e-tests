917a244d4c25dd1321fdb2a08226f267ca648b05
Mathieu Decoene <hello@mathieudecoene.be>
2018-10-16 09:27:41 +0100

    Add web component

--- src/test.e2e.ts
+++ src/test.e2e.ts
@@ -2,10 +2,10 @@ import {E2EPage, newE2EPage} from '@stencil/core/testing';
 
 describe('suite', () => {
     let page: E2EPage;
-    // let element: E2EElement;
 
     beforeEach(async () => {
-       page = await newE2EPage({ html: `<div>bla</div>`});
+        page = await newE2EPage({ html: `<test></test>`});
+        await page.waitForChanges();
     });
 
     it('test', async () => {
---
+++ src/test.tsx
@@ -0,0 +1,13 @@
+import { Component } from '@stencil/core';
+
+@Component({
+    tag: 'test'
+})
+export class Test {
+
+    render() {
+        return (
+            <div>bla</div>
+        );
+    }
+}
