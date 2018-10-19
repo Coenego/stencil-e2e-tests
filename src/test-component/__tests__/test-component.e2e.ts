65e3c780bc6e9f9a5334b8ca97e5da28fd6b05ea
Mathieu Decoene <hello@mathieudecoene.be>
2018-10-19 09:33:43 +0100

    task(tests): Add additional test

--- src/test-component/__tests__/test-component.e2e.ts
+++ src/test-component/__tests__/test-component.e2e.ts
@@ -1,6 +1,8 @@
 import { E2EElement, E2EPage, newE2EPage} from '@stencil/core/testing';
 
-describe('suite', () => {
+import { content } from '../test-component.types';
+
+describe('the component should', () => {
     let page: E2EPage;
     let elm: E2EElement;
 
@@ -9,8 +11,13 @@ describe('suite', () => {
         await page.setContent(`<test-component></test-component>`);
     });
 
-    it('test', async () => {
+    it('render correctly', async () => {
         elm = await page.find('test-component');
         expect(elm).toHaveClass('hydrated');
+    });
+
+    it('display the correct text', async () => {
+        const $content = await page.find('test-component >>> .content');
+        expect($content.innerHTML).toStrictEqual(content);
     });
 });
--- src/test-component/test-component.tsx
+++ src/test-component/test-component.tsx
@@ -1,14 +1,22 @@
 import { Component } from '@stencil/core';
 
+import { content } from './test-component.types';
+
 @Component({
     tag: 'test-component',
     shadow: true
 })
 export class TestComponent {
 
-    render() {
+    /**
+     * Render the component
+     *
+     * @return {any}
+     * @protected
+     */
+    protected render(): any {
         return (
-            <div class="test">bla</div>
+            <div class="content">{content}</div>
         );
     }
 }
